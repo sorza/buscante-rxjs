f49be411f1a4bbc976893bd43630d62eca967328
Alexandre Sorza <44484412+sorza@users.noreply.github.com>
2023-11-11 22:47:30 -0300

    Renderizando os cards de livros com base na busca

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,23 +1,23 @@
-import { LivrosResultado } from './../models/interfaces';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
+import { Item, LivrosResultado } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LivroService {
 
-  private readonly API = 'https://www.googleapis.com/books/v1/volumes?'
-
+  private readonly API = 'https://www.googleapis.com/books/v1/volumes'
   constructor(private http: HttpClient) { }
 
-  buscar(valorDigitado : string) : Observable<LivrosResultado>
-  {
-    const params = new HttpParams().append('q', valorDigitado)
-    return this.http.get<LivrosResultado>(this.API, {params})
-    .pipe(
-      tap((retornoAPI) => console.log('Fluxo do tap', retornoAPI))
+  buscar(valorDigitado: string): Observable<Item[]> {
+    const params = new HttpParams().append('q', valorDigitado )
+    return this.http.get<LivrosResultado>(this.API, { params }).pipe(
+      tap(retornoAPI => console.log('Fluxo do tap', retornoAPI)),
+      map(resultado => resultado.items),
+      tap(resultado => console.log('Fluxo após o map', resultado))
     )
   }
 }
+
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,36 +1,52 @@
-import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
 import { LivroService } from 'src/app/service/livro.service';
+import { Livro } from 'src/app/models/interfaces';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent implements OnDestroy {
+export class ListaLivrosComponent implements OnDestroy{
 
-  listaLivros: [];
+  listaLivros: Livro[];
   campoBusca: string = ''
   subscription: Subscription
+  livro: Livro
 
-  constructor(private service : LivroService) { }
+  constructor(private service: LivroService) { }
 
-  buscarLivros()
-  {
-    this.subscription = this.service.buscar(this.campoBusca).subscribe(
-      {
-        next: retornoAPI => console.log(),
-        error: erro => console.error(),
-        complete: () => console.log('Observable Completado')
-      }
+  buscarLivros() {
+    this.subscription = this.service.buscar(this.campoBusca).subscribe({
+      next: (items) => {
+        this.listaLivros = this.livrosResultadoParaLivros(items)
+      },
+      error: erro => console.error(erro),
+    }
+
     )
   }
 
-  ngOnDestroy()
-  {
+  livrosResultadoParaLivros(items): Livro[] {
+    const livros: Livro[] = []
+
+    items.forEach(item => {
+      livros.push(this.livro = {
+        title: item.volumeInfo?.title,
+        authors: item.volumeInfo?.authors,
+        publisher: item.volumeInfo?.publisher,
+        publishedDate: item.volumeInfo?.publishedDate,
+        description: item.volumeInfo?.description,
+        previewLink: item.volumeInfo?.previewLink,
+        thumbnail: item.volumeInfo?.imageLinks?.thumbnail
+      })
+    })
+
+    return livros
+  }
+
+  ngOnDestroy() {
     this.subscription.unsubscribe()
   }
 }
-
-
-
